0f4bdd5f177937be593502a09fa5327250403f80
Steve Streeting <steve@stevestreeting.com>
2021-01-22 16:23:32 +0000

    Module log tweak

--- Source/SPUD/Private/SpudModule.cpp
+++ Source/SPUD/Private/SpudModule.cpp
@@ -7,14 +7,14 @@ DEFINE_LOG_CATEGORY(LogSpudModule)
 void FSpudModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
-	UE_LOG(LogSpudModule, Log, TEXT("Steve's Persistence for Unreal Engine Module Started"))
+	UE_LOG(LogSpudModule, Log, TEXT("SPUD Module Started"))
 }
 
 void FSpudModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
-	UE_LOG(LogSpudModule, Log, TEXT("Steve's Persistence for Unreal Engine Module Stopped"))
+	UE_LOG(LogSpudModule, Log, TEXT("SPUD Module Stopped"))
 }
 
 #undef LOCTEXT_NAMESPACE
