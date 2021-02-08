3cd4fc984f7e773051704599d8d88ca8e0105410
Steve Streeting <steve@stevestreeting.com>
2021-02-08 16:43:41 +0000

    Doc update for ASpudStreamingVolume

--- Source/SPUD/Public/SpudStreamingVolume.h
+++ Source/SPUD/Public/SpudStreamingVolume.h
@@ -5,7 +5,8 @@
 
 /// Drop-in replacement for ALevelStreamingVolume which has a number of advantages:
 /// 1. Communicates with SpudSubsystem to organise the streaming in/out of levels such that they get persisted correctly
-/// 2. Linked streaming levels are editable directly in the volume, instead of the weird backwards system of pointing the level at the volume
+/// 2. Responds to both cameras and player-controlled pawns, making setup easier for 3rd person cameras (less prone to camera popping out of intuitive volumes)
+/// 3. Linked streaming levels are editable directly in the volume, instead of the weird backwards system of pointing the level at the volume
 UCLASS(Blueprintable, ClassGroup="SPUD", HideCategories=(Advanced, Attachment, Collision, Volume, Navigation))
 class SPUD_API ASpudStreamingVolume : public AVolume
 {
