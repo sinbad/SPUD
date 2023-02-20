5e601b796c3db05252142fdb7825099f8ac04344
Steve Streeting <steve@stevestreeting.com>
2023-02-20 13:07:44 +0000

    Init all primitive members of FTestAllTypesStruct

--- Source/SPUDTest/Private/TestSaveObject.h
+++ Source/SPUDTest/Private/TestSaveObject.h
@@ -39,39 +39,39 @@ struct FTestAllTypesStruct
 public:
 	// Primitive types
 	UPROPERTY(SaveGame)
-	int IntVal;
+	int IntVal = 0;
 	UPROPERTY(SaveGame)
-	uint8 UInt8Val;
+	uint8 UInt8Val = 0;
 	UPROPERTY(SaveGame)
-	uint16 UInt16Val;
+	uint16 UInt16Val = 0;
 	UPROPERTY(SaveGame)
-	uint32 UInt32Val;
+	uint32 UInt32Val = 0;
 	UPROPERTY(SaveGame)
-	uint64 UInt64Val;
+	uint64 UInt64Val = 0;
 	UPROPERTY(SaveGame)
-	int8 Int8Val;
+	int8 Int8Val = 0;
 	UPROPERTY(SaveGame)
-	int16 Int16Val;
+	int16 Int16Val = 0;
 	UPROPERTY(SaveGame)
-	int32 Int32Val;
+	int32 Int32Val = 0;
 	UPROPERTY(SaveGame)
-	int64 Int64Val;
+	int64 Int64Val = 0;
 
 	UPROPERTY(SaveGame)
-	float FloatVal;
+	float FloatVal = 0;
 	UPROPERTY(SaveGame)
-	double DoubleVal;
+	double DoubleVal = 0;
 	
 	UPROPERTY(SaveGame)
-	ETestEnum EnumVal;
+	ETestEnum EnumVal = ETestEnum::First;
 
 	// Built in structs
 	UPROPERTY(SaveGame)
-	FVector VectorVal;
+	FVector VectorVal = FVector::ZeroVector;
 	UPROPERTY(SaveGame)
-	FRotator RotatorVal;
+	FRotator RotatorVal = FRotator::ZeroRotator;
 	UPROPERTY(SaveGame)
-	FTransform TransformVal;
+	FTransform TransformVal = FTransform::Identity;
 
 	// Strings etc
 	UPROPERTY(SaveGame)
