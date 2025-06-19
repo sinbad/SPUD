c9a0ea0e2859837cd99ae7a894c15496d4a8804d
Vivian <mail@vivi.lol>
2025-06-18 22:40:50 -0700

    Add support for using the SaveGameSystem on platforms with a restricted filesystem

--- Source/SPUD/SPUD.Build.cs
+++ Source/SPUD/SPUD.Build.cs
@@ -1,11 +1,28 @@
 using UnrealBuildTool;
 using System.IO;
+// ReSharper disable ConditionIsAlwaysTrueOrFalse
 
 public class SPUD : ModuleRules
 {
 	public SPUD(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		const bool ForceUseSaveGameSystem = false;
+
+		// We have to use the SaveGameSystem if the Target Platform
+		// isn't a known platform with an unrestricted filesystem
+		if (
+			!(
+			    (Target.Platform == UnrealTargetPlatform.Win64) ||
+			    (Target.Platform == UnrealTargetPlatform.Mac) ||
+			    (Target.Platform == UnrealTargetPlatform.Linux) ||
+			    (Target.Platform == UnrealTargetPlatform.LinuxArm64) 
+		    ) || ForceUseSaveGameSystem
+		)
+		{
+			PrivateDefinitions.Add("USE_SAVEGAMESYSTEM=1");
+		}
 
         PublicIncludePaths.AddRange(
             new string[] {
