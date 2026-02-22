981faa0814f7f9c664eb4f0364128794bb14140b
Steve Streeting <steve@stevestreeting.com>
2026-02-22 11:27:32 +0000

    Bump save version and only load ClassID from new saves to avoid breaking old saves

--- Source/SPUD/Private/SpudData.cpp
+++ Source/SPUD/Private/SpudData.cpp
@@ -10,7 +10,7 @@
 DEFINE_LOG_CATEGORY(LogSpudData)
 
 // System version covers our internal format changes
-#define SPUD_CURRENT_SYSTEM_VERSION 3
+#define SPUD_CURRENT_SYSTEM_VERSION 4
 
 // int32 so that Blueprint-compatible. 2 billion should be enough anyway and you can always use the negatives
 int32 GCurrentUserDataModelVersion = 0;
@@ -397,7 +397,11 @@ void FSpudNamedObjectData::ReadFromArchive(FSpudChunkedDataArchive& Ar, uint32 S
 {
 	if (ChunkStart(Ar))
 	{
-		Ar << ClassID;
+		// ClassID was added in save version 4
+		if (StoredSystemVersion >= 4)
+		{
+			Ar << ClassID;
+		}
 		Ar << Name;
 		CoreData.ReadFromArchive(Ar, StoredSystemVersion);
 		Properties.ReadFromArchive(Ar, StoredSystemVersion);
